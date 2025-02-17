ebddb85d5415a9d0719a37500f39e0b45117895c
Maartje Eyskens <maartje@eyskens.me>
2025-02-17 12:11:06 +0100

    use 100px wide wbmp

--- img-to-wbmp.sh
+++ img-to-wbmp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-magick "$1" -resize 120 -monochrome +dither output.bmp
-magick output.bmp -resize 120 output.wbmp
+magick "$1" -resize 100 -monochrome +dither output.bmp
+magick output.bmp -resize 100 output.wbmp
