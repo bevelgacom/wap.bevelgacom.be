0d309b6ef399d658f96e7acf9fe5aa4bef2e78de
Maartje Eyskens <maartje@eyskens.me>
2025-03-02 17:41:43 +0100

    also save png of converted images for soon dynamic switch to png on color phones

--- img-to-wbmp.sh
+++ img-to-wbmp.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-magick "$1" -resize 100 -monochrome +dither output.bmp
-#magick "$1" -resize 100 -dither FloydSteinberg -remap pattern:gray50 output.bmp
 
-magick output.bmp -resize 100 output.wbmp
+NEW_NAME=$(echo "$1" | cut -f 1 -d '.')
+
+
+
+magick "$1" -resize 100 -monochrome +dither "$NEW_NAME.bmp"
+#magick "$1" -resize 100 -dither FloydSteinberg -remap pattern:gray50 $NEW_NAME.bmp
+
+magick "$NEW_NAME.bmp" -resize 100 "$NEW_NAME.wbmp"
+
+# For dynamic png/wbmp switching code
+magick "$1" -resize 100 "$NEW_NAME.png"
