bbb562b073d0c84cf0a570760388c42606a36ae2
Maartje Eyskens <maartje@eyskens.me>
2025-03-30 14:31:58 +0200

    Fix news

--- link-cache.go
+++ link-cache.go
@@ -1,24 +1,32 @@
 package main
 
+import "math/rand"
+
 // NOTE: this is not at all scalable, this should become a database soon
 
 // linkCache is a map of link IDs to their URLs
 // this is so we can query cache:linkID and fetch the full URL
 // this is done as the Nokia 7110 has a hard link length limit
 var linkCache = map[string]string{}
+var linkToID = map[string]string{}
 
 func generateID() string {
 	// generate a random 8 character string
-	rand := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	randRunes := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	id := ""
 	for i := 0; i < 8; i++ {
-		id += string(rand[i])
+		// pick random character
+		id += string(randRunes[rand.Intn(len(randRunes))])
 	}
 
 	return id
 }
 
 func StoreLink(link string) string {
+	if _, exists := linkToID[link]; exists {
+		return linkToID[link]
+	}
+
 	id := generateID()
 	_, exists := linkCache[id]
 	for exists {
@@ -26,6 +34,7 @@ func StoreLink(link string) string {
 		_, exists = linkCache[id]
 	}
 	linkCache[id] = link
+	linkToID[link] = id
 	return id
 }
 
